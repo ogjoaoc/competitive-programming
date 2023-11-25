464ba0e71de13b58c72f4dcc596c74e1b5cb6f4f
ogjoaoc <joaocarlosgoncalvesfilho@gmail.com>
2023-11-25 02:17:24 -0300

    file update

---
+++ code_forces_beginner/A_3.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    string t;
+    char letter;
+    int sz;
+    cin >> t;
+
+    set<char> my_set(t.begin(), t.end());
+    sz = my_set.size();
+
+    if (sz % 2 == 0) {
+        cout << "CHAT WITH HER!";
+    }
+    else {
+        cout << "IGNORE HIM!";
+    }
+
+    return 0;
+}
