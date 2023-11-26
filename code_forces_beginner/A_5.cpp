a07becdab7aad1fb1f491d908483918168f246aa
ogjoaoc <joaocarlosgoncalvesfilho@gmail.com>
2023-11-26 14:16:25 -0300

    file update

--- code_forces_beginner/A_5.cpp
+++ code_forces_beginner/A_5.cpp
@@ -9,14 +9,11 @@ int main() {
     int n, m, contador = 0;
     cin >> n >> m;
     int a, b;
-    int eq1, eq2;
     int sum = n + m;
 
     for (a = 0; a < sum; a++) {
         for (b = 0; b < sum; b++) {
-            eq1 = pow(a, 2) + b;
-            eq2 = a + pow(b, 2);
-            if (eq1 == n && eq2 == m) {
+            if ((pow(a, 2) + b) == n && (a + pow(b, 2)) == m) {
                 contador++;
             }
         }
