76e491b16f6c0ae5df413b44952ded7802092f99
ogjoaoc <joaocarlosgoncalvesfilho@gmail.com>
2023-12-28 02:30:29 -0300

    file update

--- st_notebook/bixecamp/aula_1/7.cpp
+++ st_notebook/bixecamp/aula_1/7.cpp
@@ -1,2 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int main() {
+
+    int t, ans;
+    int x, y;
+    cin >> t;
+    for (int i = 0; i<t+1; i++) {
+        cin >> x >> y;
+    }
+
+    ans = (t-4)/2;
+    cout << ans << "\n";
+
+    return 0;
+}
