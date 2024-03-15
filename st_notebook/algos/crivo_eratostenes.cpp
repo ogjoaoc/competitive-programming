53cae87eeb001c0c0661722f11a1715041fadd11
ogjoaoc <joaocarlosgoncalvesfilho@gmail.com>
2024-03-15 16:19:30 -0300

    add: crivo de eratóstenes

---
+++ st_notebook/algos/crivo_eratostenes.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+ 
+using namespace std; 
+
+int eh_composto[10000007]; // 1 (true) = composto
+
+void crivo(int x){
+    eh_composto[1] = 1;
+    if(!eh_composto[x]) { // possivel primo
+        for(int i=2; i<=x; i++) {
+            if(!eh_composto[i]) { // sub-valor de x é um possível primo
+                for(int j=2; j*i <= x; j++) {
+                    eh_composto[i*j] = 1;
+                } 
+            }
+        }
+    }
+    return;
+}
+ 
+int main() 
+{ 
+    int n; 
+    cin >> n;
+    crivo(n);
+    for(int i=1; i<=n; i++){
+        if(!eh_composto[i]){
+            cout << i << " "; 
+        }
+    }
+    cout << "\n";
+    return 0;
+}
