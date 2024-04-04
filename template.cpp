2ef5990da803fe8256e07b4ceadb1b8bcceabcb7
ogjoaoc <joaocarlosgoncalvesfilho@gmail.com>
2024-04-03 23:12:38 -0300

    add template

---
+++ template.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+    
+using namespace std;
+    
+typedef long long int ll;
+typedef tuple<int,int,int> trio;
+typedef pair<ll,ll> pii;
+const int MAXN = (1e5)*2;
+const int INF = (int) 1e9 + 10;
+const int MOD = 1e9+7;
+#define int long long 
+#define all(x) x.begin(), x.end()
+#define vi vector<int>
+#define ss second
+#define ff first
+#define mp make_pair
+#define pb push_back
+    
+
+void solve() {
+    
+    
+}
+int32_t main() {
+    
+    ios::sync_with_stdio(false); 
+    cin.tie(0); 
+    int tt = 1;
+    //cin >> tt;
+    
+    while(tt--) {
+        solve();
+    }
+    
+}
