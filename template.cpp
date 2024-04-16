8795c8f7fe6d854fc8b9445ed26774edff71f05a
ogjoaoc <joaocarlosgoncalvesfilho@gmail.com>
2024-04-16 15:16:47 -0300

    update template for sublime

--- template.cpp
+++ template.cpp
@@ -1,35 +1,41 @@
 #include <bits/stdc++.h>
-    
 using namespace std;
-    
-typedef long long int ll;
-typedef tuple<int,int,int> trio;
-typedef pair<ll,ll> pii;
-const int MAXN = (1e5)*2;
-const int INF = (int) 1e9 + 10;
-const int MOD = 1e9+7;
-#define int long long 
-#define all(x) x.begin(), x.end()
-#define vi vector<int>
-#define ss second
-#define ff first
+#define pii pair<int,int>
+#define iii tuple<int, int, int> 
 #define mp make_pair
+#define ll long long
+#define ff first
+#define ss second
 #define pb push_back
-    
+#define all(x) x.begin(), x.end()
+#define _ ios::sync_with_stdio(false); cin.tie(0);
+
+const int MAXN = 1005;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const int MOD = 1e9+7;
+
 
 void solve() {
-    
-    
+
+	cout << "Hello World!\n";
+
 }
-int32_t main() {
+ 
+int main() {_ 
+
+    #ifndef ONLINE_JUDGE
+    freopen("in1.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    #endif
     
-    ios::sync_with_stdio(false); 
-    cin.tie(0); 
     int tt = 1;
     //cin >> tt;
-    
     while(tt--) {
         solve();
     }
-    
+ 
 }
+
+
+
