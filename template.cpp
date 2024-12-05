3e7382088b8554eae85745829bff2a8cd4b7a6a9
ogjoaoc <joaocarlosgoncalvesfilho@gmail.com>
2024-12-05 00:26:03 -0300

    upd template

--- template.cpp
+++ template.cpp
@@ -1,41 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define pii pair<int,int>
-#define iii tuple<int, int, int> 
-#define mp make_pair
-#define ll long long
-#define ff first
-#define ss second
-#define pb push_back
-#define all(x) x.begin(), x.end()
-#define _ ios::sync_with_stdio(false); cin.tie(0);
 
-const int MAXN = 1005;
-const int INF = 0x3f3f3f3f;
-const ll LINF = 0x3f3f3f3f3f3f3f3fll;
-const int MOD = 1e9+7;
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#else
+#define db(...)
+#endif
 
+#define int long long
+using pii = pair<int,int>;
+const int oo = 1e18;
 
 void solve() {
-
-	cout << "Hello World!\n";
-
-}
- 
-int main() {_ 
-
-    #ifndef ONLINE_JUDGE
-    freopen("in1.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
-    #endif
-    
-    int tt = 1;
-    //cin >> tt;
-    while(tt--) {
-        solve();
-    }
- 
 }
 
-
-
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int tc = 1;
+    cin >> tc;
+    while(tc--) solve();
+    return 0;
+}
