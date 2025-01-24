df9e743bee790d75b9d820b5553f6fe511d80b77
João Carlos <110072806+ogjoaoc@users.noreply.github.com>
2025-01-23 21:50:22 -0300

    update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,23 +1,19 @@
 #include <bits/stdc++.h>
-using namespace std;
-
-#ifndef ONLINE_JUDGE
-#include "debug.h"
-#else
-#define db(...)
-#endif
-
 #define int long long
-using pii = pair<int,int>;
-const int oo = 1e18;
+
+using namespace std;
 
 void solve() {
 }
 
 int32_t main() {
-    cin.tie(0)->sync_with_stdio(0);
-    int tc = 1;
-    cin >> tc;
-    while(tc--) solve();
-    return 0;
+  cin.tie(0)->sync_with_stdio(0);
+
+  int t; cin >> t;
+  
+  while(t--) {
+    solve();
+  }
+
+  return 0;
 }
